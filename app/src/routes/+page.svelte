5b4342ffcec3ee6cae8c13ea21751fac0db1bb0e
HaGeza <pelokbalazs@gmail.com>
2026-03-05 21:37:44 +0100

    card hover and grid

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -2,10 +2,20 @@
 	<h1 class="title">Welcome to my Blog</h1>
 	<h2 class="subtitle">I will put stuff here (hopefully)</h2>
 
-	<div class="mt-8">
-		<article class="card">
+	<div class="card-grid mt-8">
+		<div class="card">
 			<h3 class="subtitle">Dummy Stuff</h3>
 			<p class="description">Unbelievable stuff</p>
-		</article>
+		</div>
+
+		<div class="card">
+			<h3 class="subtitle">Dummy Stuff</h3>
+			<p class="description">Unbelievable stuff</p>
+		</div>
+
+		<div class="card">
+			<h3 class="subtitle">Dummy Stuff</h3>
+			<p class="description">Unbelievable stuff</p>
+		</div>
 	</div>
 </div>
