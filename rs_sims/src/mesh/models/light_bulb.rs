b53258b057606ff57a18cd1b2fc09fd65394e6a3
HaGeza <pelokbalazs@gmail.com>
2026-06-14 22:58:30 +0200

    tmp

--- rs_sims/src/mesh/models/light_bulb.rs
+++ rs_sims/src/mesh/models/light_bulb.rs
@@ -1,11 +1,25 @@
-use macroquad::models::Mesh;
+use macroquad::{
+    math::{Vec2, vec2},
+    models::Mesh,
+};
 
-use crate::mesh::common::combine_meshes;
+use crate::mesh::{common::combine_meshes, lathe_mesh::create_lathe_mesh};
 
 const GLASS_SEGMENTS_TO_RINGS_RATIO: f32 = 1.;
 
+fn create_light_bulb_glass_profile() -> Vec<Vec2> {
+    let semicircle: Vec<Vec2> = get_semicircle();
+
+    let curve_a = semicircle.last()?;
+    let curve_b = get_vec2_in_direction(semicircle[semicircle.len() - 2], curve_a, 2.);
+    let curve_c = vec2(curve_b.x, curve_b.y - curve_a.distance(curve_b));
+    let quadratic_bezier: Vec<Vec2> = get_quadratic_bezier(curve_a, curve_b, curve_c);
+
+    [semicircle[..semicircle.len() - 1], &quadratic_bezier].concat()
+}
+
 fn create_light_bulb_glass_mesh(num_rings: u16) -> Mesh {
-    let profile = create_light_bulb_glass_profile();
+    create_lathe_mesh(&create_light_bulb_glass_profile(), num_rings)
 }
 
 const CAP_SEGMENTS_TO_RINGS_RATIO: f32 = 1.;
